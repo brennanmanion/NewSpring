ac32087d81feb8700bc7a5c82743cfbbfb5258e2
brennanmanion <brennanman007@gmail.com>
2022-01-24 22:07:41 -0700

    http.csrf().disable(); allowed me to make post requests. Still need to figure out rest easy validation

--- src/main/java/com/mycompany/configurations/SecurityConfiguration.java
+++ src/main/java/com/mycompany/configurations/SecurityConfiguration.java
@@ -23,11 +23,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/admin").hasRole("ADMIN")
+//			.antMatchers("/user").hasAnyRole("ADMIN","USER")
+//			.antMatchers("/").permitAll()
+//			.and().formLogin();
 		http.authorizeRequests()
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("ADMIN","USER")
-			.antMatchers("/").permitAll()
-			.and().formLogin();
+		.antMatchers("/*").permitAll();
+		
+		http.csrf().disable();
 	}
 	
 	@Bean
